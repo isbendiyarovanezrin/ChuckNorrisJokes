65c42d8fae965d0c50b7f86d05c103edea37bbac
Nəzrin <isbndyrva@gmail.com>
2022-01-04 15:43:51 +0400

    Add Chuck Norris joke generator

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const btn = document.getElementById("btn");
+const p = document.querySelector(".joke");
+const img = document.querySelector(".img");
+
+btn.addEventListener("click", getRandomJoke);
+
+function getRandomJoke() {
+  fetch("https://api.chucknorris.io/jokes/random")
+    .then((response) => response.json())
+    .then((data) => {
+      p.textContent = data.value;
+      img.src = data.icon_url;
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
